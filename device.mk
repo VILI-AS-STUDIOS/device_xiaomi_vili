5acb9fd403e2fb1c2339e49dc610c1583e542dfd
Carlos Arriaga <37526095+TheStrechh@users.noreply.github.com>
2023-12-24 06:52:19 -0600

    redwood: Import surfaceflinger props
    * Relax timers for automatic refresh rate switching
    * Based on https://github.com/LineageOS/android_device_xiaomi_sweet/commit/a8782400ef45b42a87987bfb2a186704dfe9a07a

--- device.mk
+++ device.mk
@@ -44,5 +44,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
+    ro.surface_flinger.set_display_power_timer_ms=1000
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/redwood/redwood-vendor.mk)
